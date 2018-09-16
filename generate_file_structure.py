da70ff714bd147633759c79216210619e2c5d8bc
Leo Q <leoq91@gmail.com>
2018-09-15 22:39:25 -0700

    edit folder structure

--- generate_file_structure.py
+++ generate_file_structure.py
@@ -2,14 +2,14 @@ import os
 import errno
 from shutil import rmtree, copytree
 
+
 def generate_file_structure():
     name = raw_input("What is the name for files? -> ")
 
     if os.path.isdir("outputFiles"):
-        rmtree('outputFiles') 
+        rmtree('outputFiles')
     try:
         copytree("template/init", "outputFiles/" + name)
-        
 
     except OSError as e:
         if e.errno != errno.EEXIST:
--- outputFiles/asdfa/runs/NAME_run_001_TUFLOW.bat
+++
@@ -1,5 +0,0 @@
-Set TF_Exe="C:\Program Files\Tuflow_w64\TUFLOW_iSP_w64.exe"
-Set RUN=start "TUFLOW" %TF_Exe%
-
-%RUN% "NAME_001.tcf" 
-pause
